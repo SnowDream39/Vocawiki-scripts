8e9aaf66d173729f114d630b55af448e2d9e0900
SnowDream39 <759723417@qq.com>
2025-09-18 19:16:23 +0800

    first commit

---
+++ producer-works.js
@@ -0,0 +1,167 @@
+const categoriesMap = {
+  "Animator": "PV",
+  "Arranger": "编曲",
+  "Composer": "作曲",
+  "Distributor": "发行",
+  "EffectiveVocalist": "和声",
+  "Illustrator": "曲绘",
+  "Instrumentalist": "演奏",
+  "Lyricist": "作词",
+  "Mastering": "母带",
+  "Mixer": "混音",
+  "Other": "其他",
+  "Producer": "词·曲",
+  "Publisher": "出版",
+  "VocalDataProvider": "音源",
+  "Vocalist": "演唱",
+  "VoiceManipulator": "调教"
+}
+
+const platformIconsMap = {
+  "Youtube": "https://vj-wiki.top/images/f/f5/Bilibili_Icon.svg",
+  "Bilibili": "https://vj-wiki.top/images/f/f5/Bilibili_Icon.svg",
+  "SoundCloud": "https://vj-wiki.top/images/f/f5/Bilibili_Icon.svg",
+  "NicoNicoDouga": "https://vj-wiki.top/images/f/f5/Bilibili_Icon.svg",
+}
+
+// =========== 小工具函数 =========
+
+/**
+ * 
+ * @param {any} obj 
+ * @param {string} key 
+ * @param {unknown} value 
+ */
+
+function addToGroup(obj, key, value) {
+  if (!obj[key]) {
+    obj[key] = []
+  }
+  obj[key].push(value)
+}
+
+// =========== API 调用 ===========
+
+async function fetchArtistData(artistId) {
+  const response = await fetch(`https://vocadb.net/api/artists/${artistId}?lang=Default`);
+  return await response.json();
+}
+
+async function fetchArtistSongsData(artistId, page = 1, pageSize = 10) {
+  const response = await fetch(`https://vocadb.net/api/songs?start=${pageSize * (page-1)}&getTotalCount=true&maxResults=${pageSize}&query=&fields=Artists,MainPicture,PVs&lang=Default&nameMatchMode=Auto&sort=PublishDate&childTags=false&artistId%5B%5D=${artistId}&artistParticipationStatus=Everything&songTypes=Original,Remaster,Remix,Cover&onlyWithPvs=true`);
+  return await response.json();
+}
+
+// =========== 业务逻辑 ===========
+
+/**
+ * 生成一份artist列表
+ * @param {*} artists 
+ * @returns any[]
+ */
+function makeStaff(artists) {
+  let roles
+  const staff = {}
+  const producers = []
+  for (const artist of artists) {
+    if (artist.categories === 'Other') {
+      roles = artist.effectiveRoles
+    } else if (artist.categories === 'Vocalist') {
+      roles = 'Vocalist'
+    } else if (artist.categories.includes('Producer')) {
+      producers.push(artist.name)
+      if (artist.effectiveRoles === 'Default') {
+        roles = 'Producer' // 词·曲
+      } else {
+        roles = artist.effectiveRoles
+      }
+    } else {
+      roles = artist.categories
+    }
+    for (const role of roles.split(', ')) {
+      addToGroup(staff, role, artist.name)
+    }
+  }
+  staff.Vocalist = Array.from(new Set(staff.Vocalist))
+  // 以上是原来的逻辑，生成一个object。接下来把它变成一个数组
+  const staffArray = []
+  for (let [key, value] of Object.entries(staff)) {
+    staffArray.push({
+      role: categoriesMap[key], 
+      names: value
+    })    
+  }
+
+  return staffArray
+}
+
+/**
+ * 去掉不符合标准的条目，并添加一些额外信息
+ * @param {Array} songsData
+ */
+function modifySongsData(songsData) {
+  // ========== 根据PV筛选歌曲 ===========
+  songsData.forEach(song => {
+    // 去掉YouTube自动生成视频
+    song.pvs = song.pvs.filter(pv => pv.author.slice(-5) != 'Topic');
+  })
+  // 去掉没有任何PV的视频
+  songsData = songsData.filter(song => song.pvs.length > 0);
+
+
+  // ========== 添加额外信息 ===========
+  // 生成staff列表
+  for (const song of songsData) {
+    song.artistsArray = makeStaff(song.artists);
+    for (const [index, artist] of song.artistsArray.entries()) {
+      if (artist.role === '演唱') {
+        song.vocalists = artist.names;
+        song.artistsArray.splice(index, 1);
+      }
+    }
+  }
+  songsData.forEach(song => {
+    // 为每个PV添加图标
+    song.pvs.forEach(pv => {pv.icon = platformIconsMap[pv.service] || ''});
+    // 选出一个最合适的缩略图
+    song.bestThumbnailUrl = song.pvs.filter(pv => pv.service === 'NicoNicoDouga')[0]?.thumbUrl;
+  })
+  return songsData;
+}
+document.addEventListener('alpine:init', () => {
+  Alpine.data('artistData', () => ({
+    name: '',
+    songs: [],
+
+    async init() {
+      function render(object, songsData) {
+        object.songs = songsData.map(song => ({
+          id: song.id,
+          name: song.name,
+          publishDate: song.publishDate ? (new Date(song.publishDate)).toLocaleDateString() : '未知',
+          url: `https://vocadb.net/S/${song.id}`,
+          artists: song.artistsArray,
+          pvs: song.pvs,
+          thumb: song.bestThumbnailUrl,
+          vocalists: song.vocalists || [],
+        }));
+      }
+
+      this.name = await fetchArtistData(110656).then(data => data.name);
+
+      let songsData = [];
+      let total = 1000;
+      let page = 1;
+      const pageSize = 10;
+
+      while (pageSize * (page - 1) < total) {
+        const data = await fetchArtistSongsData(110656, page);
+        total = data.totalCount;
+        const newItems = modifySongsData(data.items)
+        songsData.push(...newItems);
+        page++;
+        render(this, songsData);
+      }
+    }
+  }))
+});
